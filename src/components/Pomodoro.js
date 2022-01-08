44fa6cf6608be3f716e750b6519afa50ecae90c1
Zach Alvstad <52245667+zda17@users.noreply.github.com>
2022-01-08 09:48:01 -0600

    add widths for all screen sizes

--- src/components/Pomodoro.js
+++ src/components/Pomodoro.js
@@ -4,7 +4,7 @@ import Tasks from "./Tasks";
 
 const Pomodoro = () => {
     return (
-        <div className="pomodoro w-10/12 flex flex-col py-8">
+        <div className="pomodoro sm:w-10/12 md:w-10/12 lg:w-1/5 xl:w-1/5 2xl:w-1/5 flex flex-col py-8">
             <div className="h-16 flex justify-center">
                 <img 
                     src="https://termly.io/wp-content/themes/genesis-tly-v4/images/logo.svg" 
