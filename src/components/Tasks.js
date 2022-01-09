c6bf7b4aecbd7868711010d5634cc689c4230ca5
Zach Alvstad <52245667+zda17@users.noreply.github.com>
2022-01-09 07:47:41 -0600

    move confetti function to its own file

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -4,6 +4,10 @@ import Task from './Task';
 import AddTask from './AddTask';
 import ProgressBar from './ProgressBar';
 
+// I copied this "confetti" code from an external source, so moving it out of the way
+// just thought it would be a nice creative touch upon task completion
+import confetti from '../methods/Confetti.js'
+
 // TODO: move tracking progress state up one level from ProgressBar.js to here, so that I can use
 // it in Timer.js to display different alerts. Then track current task for display inside of Alerts.
 // I could also track the start time and end time of each task, then calculate how long that task
@@ -14,85 +18,6 @@ import ProgressBar from './ProgressBar';
 const Tasks = ({ toggleTimer, timerActive }) => {
     const [tasks, setTasks] = useState([]);
     const [progress, setProgress] = useState(0);
-
-    const clearConfetti = () => {
-        const canvas = document.querySelector('#confetti');
-        canvas.style.display = "none";
-    }
-
-    // I copied this "confetti" function from an external library, so I'm collapsing it for space's sake
-    const confetti = () => {
-        const canvas = document.querySelector('#confetti');
-
-        canvas.style.display = "block";
-
-        const w = canvas.width = window.innerWidth;
-        const h = canvas.height = window.innerHeight * 2;
-
-        function loop() {
-            requestAnimationFrame(loop);
-                ctx.clearRect(0,0,w,h);
-            
-            confs.forEach((conf) => {
-                conf.update();
-                conf.draw();
-            })
-        }
-
-        function Confetti () {
-            //construct confetti
-            const colours = ['#fde132', '#009bde', '#ff6b00'];
-            
-            this.x = Math.round(Math.random() * w);
-            this.y = Math.round(Math.random() * h)-(h/2);
-            this.rotation = Math.random()*360;
-
-            const size = Math.random()*(w/60);
-            this.size = size < 15 ? 15 : size;
-
-            this.color = colours[Math.floor(colours.length * Math.random())];
-            this.speed = this.size/7;
-            this.opacity = Math.random();
-            this.shiftDirection = Math.random() > 0.5 ? 1 : -1;
-        }
-
-        Confetti.prototype.border = function() {
-            if (this.y >= h) {
-                this.y = h;
-            }
-        }
-
-        Confetti.prototype.update = function() {
-        this.y += this.speed;
-        
-        if (this.y <= h) {
-            this.x += this.shiftDirection/3;
-            this.rotation += this.shiftDirection*this.speed/100;
-        }
-
-        if (this.y > h) 
-            this.border();
-        };
-
-        Confetti.prototype.draw = function() {
-            ctx.beginPath();
-            ctx.arc(this.x, this.y, this.size, this.rotation, this.rotation+(Math.PI/2));
-            ctx.lineTo(this.x, this.y);
-            ctx.closePath();
-            ctx.globalAlpha = this.opacity;
-            ctx.fillStyle = this.color;
-            ctx.fill();
-        };
-
-        const ctx = canvas.getContext('2d');
-        const confNum = Math.floor(w / 4);
-        const confs = new Array(confNum).fill().map(_ => new Confetti());
-
-        loop();
-
-        // clear confetti after 10 seconds so user can continue using the app.
-        setTimeout(clearConfetti, 10000);
-    }
 
     useEffect(() => {
         const getTasks = async () => {
